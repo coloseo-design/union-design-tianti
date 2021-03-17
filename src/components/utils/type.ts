d5c12444e762ddaeef66615bc6bc6192dbfaa4b2
jc-dengp <jc-dengp@chinaunicom.cn>
2021-03-17 18:10:24 +0800

    fix: 🐛 修复eslint的错误

--- src/components/utils/type.ts
+++ src/components/utils/type.ts
@@ -1,15 +1,13 @@
 export const tuple = <T extends string[]>(...args: T) => args;
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-
 export interface BaseProps {
   /* class名称 */
   className?: string;
   /* 自定义类前缀 */
   prefixCls?: string;
   /* 屏幕 < 576px 响应式栅格 */
-};
-
+}
 
 export interface ColProps {
   /* 内容所占的栅格数 */
@@ -18,7 +16,7 @@ export interface ColProps {
   offset?: number;
   /** 栅格顺序 */
   order?: number;
-};
+}
 
 export interface ResponsiveColProps extends ColProps {
   xs?: number | ColProps;
