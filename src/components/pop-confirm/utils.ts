af8e4b45f153231e10b00db658dd4f030546e16b
jc-dengp <jc-dengp@chinaunicom.cn>
2021-03-17 18:10:24 +0800

    fix: 🐛 修复eslint的错误

--- src/components/pop-confirm/utils.ts
+++ src/components/pop-confirm/utils.ts
@@ -6,29 +6,30 @@ interface ChangeD {
 }
 
 export const changeTopDir: ChangeD = {
-  'top': 'bottom',
-  'topLeft': 'bottomLeft',
-  'topRight': 'bottomRight',
+  top: 'bottom',
+  topLeft: 'bottomLeft',
+  topRight: 'bottomRight',
 };
 
 export const changeLeftDir: ChangeD = {
-  'left': 'right',
-  'leftTop': 'rightTop',
-  'leftBottom': 'rightBottom',
+  left: 'right',
+  leftTop: 'rightTop',
+  leftBottom: 'rightBottom',
 };
 export const changeRightDir: ChangeD = {
-  'right': 'left',
-  'rightTop': 'leftTop',
-  'rightBottom': 'leftBottom',
-}
+  right: 'left',
+  rightTop: 'leftTop',
+  rightBottom: 'leftBottom',
+};
 export const changeBottomDir: ChangeD = {
-  'bottom': 'top',
-  'bottomLeft': 'topLeft',
-  'bottomRight': 'topRight',
+  bottom: 'top',
+  bottomLeft: 'topLeft',
+  bottomRight: 'topRight',
 };
 
 export function uuid() {
   function S4() {
+    /* eslint no-bitwise: 0 */
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   }
   return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
