51745b4f3e6faef8241250e33326394c62882bc1
facedan <dan.yu@coloseo.cn>
2021-03-19 17:27:57 +0800

    feat: 🎸 statistic组件完成

--- src/site/demos/index.ts
+++ src/site/demos/index.ts
@@ -3,3 +3,4 @@
 /* eslint-disable */export { default as Layout } from '../../components/layout/demo';
 /* eslint-disable */export { default as Modal } from '../../components/modal/demo';
 /* eslint-disable */export { default as PopConfirm } from '../../components/pop-confirm/demo';
+/* eslint-disable */export { default as Statistic } from '../../components/statistic/demo';
--- src/site/docs/index.ts
+++ src/site/docs/index.ts
@@ -1,10 +1,12 @@
 /* eslint-disable */
 export { default as Button } from './button/button';
 /* eslint-disable */
+export { default as Icon } from './icon/icon';
+/* eslint-disable */
+export { default as Layout } from './layout/layout';
+/* eslint-disable */
 export { default as Modal } from './modal/modal';
 /* eslint-disable */
 export { default as PopConfirm } from './pop-confirm/pop-confirm';
 /* eslint-disable */
-export { default as Icon } from './icon/icon';
-/* eslint-disable */
-export { default as Layout } from './layout/layout';
+export { default as Statistic } from './statistic/statistic';
