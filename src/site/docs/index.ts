cbbeaeb099f1299955a7a309445f8eb639f707ec
jc-wub1 <jc-wub1@chinaunicom.cn>
2021-03-23 16:00:58 +0800

    feat: 🎸 modal消息提示完成

--- src/site/docs/index.ts
+++ src/site/docs/index.ts
@@ -1,8 +1,6 @@
 /* eslint-disable */
 export { default as Button } from './button/button';
 /* eslint-disable */
-export { default as Card } from './card/card';
-/* eslint-disable */
 export { default as Checkbox } from './checkbox/checkbox';
 /* eslint-disable */
 export { default as Icon } from './icon/icon';
@@ -15,10 +13,12 @@ export { default as PopConfirm } from './pop-confirm/pop-confirm';
 /* eslint-disable */
 export { default as Result } from './result/result';
 /* eslint-disable */
-export { default as Statistic } from './statistic/statistic';
-/* eslint-disable */
-export { default as Table } from './table/table';
-/* eslint-disable */
 export { default as Tree } from './tree/tree';
 /* eslint-disable */
 export { default as TreeSelect } from './tree-select/tree-select';
+/* eslint-disable */
+export { default as Table } from './table/table';
+/* eslint-disable */
+export { default as Card } from './card/card';
+/* eslint-disable */
+export { default as Statistic } from './statistic/statistic';
