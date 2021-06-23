c5718bae891e783a8643d209ca502dfadc0f2165
jc-lvhj <jc-lvhj@chinaunicom.cn>
2021-06-23 18:03:41 +0800

    修复table筛选条件按钮颜色

--- src/components/table/header.tsx
+++ src/components/table/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ColumnsProps } from './type';
 import {
   Icon,
@@ -20,6 +20,7 @@ const TableHeader: React.FC<TableHeaderProps> = (props: TableHeaderProps) => {
   const onChange = (name: string) => (_values: string[]) => {
     props.onChange && props.onChange(name, _values);
   };
+  const [visible, setVisible] = useState(false);
   return (
     <thead>
       <tr>
@@ -44,6 +45,9 @@ const TableHeader: React.FC<TableHeaderProps> = (props: TableHeaderProps) => {
                   { column.filters && column.filters?.length > 0 ? (
                     <Dropdown
                       placement="bottomCenter"
+                      onVisibleChange={(isVisible) => {
+                        setVisible(isVisible);
+                      }}
                       overlay={(
                         <Filter
                           prefix={prefixCls}
@@ -54,7 +58,12 @@ const TableHeader: React.FC<TableHeaderProps> = (props: TableHeaderProps) => {
                       )}
                       trigger={['click']}
                     >
-                      <Icon type="sizer" style={{ marginLeft: 5, display: 'inline-block', width: 20 }} />
+                      <Icon
+                        type="sizer"
+                        style={{
+                          marginLeft: 5, display: 'inline-block', width: 20, color: visible ? '#207EEA' : '#ACAFB9',
+                        }}
+                      />
                     </Dropdown>
                   ) : null}
                 </span>
