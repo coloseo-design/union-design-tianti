0e9c10a469ffada14a410a8ac70cd634ef7c672e
余丹 <yudan@yudandeMacBook-Pro.local>
2021-07-22 12:01:37 +0800

    面包屑更改

--- src/components/breadcrumb/item.tsx
+++ src/components/breadcrumb/item.tsx
@@ -1,6 +1,5 @@
 import React, { Component, ReactNode } from 'react';
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
 import { ConfigConsumer, ConfigConsumerProps } from '../config-provider';
 
 export interface ItemProps extends React.HTMLAttributes<HTMLSpanElement> {
@@ -20,9 +19,9 @@ class Item extends Component<ItemProps> {
       prefixCls, children, className, separator, href, onClick, ...rest
     } = this.props;
     const prefix = getPrefixCls('breadcrumb-item', prefixCls);
-    const mainClass = classNames(prefix, className, {
+    const mainClass = classNames(prefix, {
       // [`${prefix}-has-sider`]: siders.length > 0,
-    });
+    }, className);
 
     const onclick = (e: unknown) => {
       if (onClick) {
@@ -33,7 +32,7 @@ class Item extends Component<ItemProps> {
     return (
       <span {...rest} className={mainClass}>
         <span className={`${prefix}-link`} onClick={onclick}>
-          {href ? <Link to={href}>{children}</Link> : children}
+          {href ? <a href={href}>{children}</a> : children}
         </span>
         <span className={`${prefix}-separator`}>{separator}</span>
       </span>
