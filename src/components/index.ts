b3ab6ef880b7497eaaafcd9ec2f97de15d2d782a
liao.yj <liao.yingju@cyberway.net.cn>
2022-01-19 18:01:42 +0800

    fix: 🐛 样式变量重名覆盖 fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -16,61 +16,64 @@ if (
 
 // 请在此处导出所有组件
 export { default as Affix } from './affix';
-export { default as Button } from './button';
-export { default as Card } from './card';
-export { default as Tag } from './tag';
-export { default as Tabs } from './tabs';
-
-export { default as Layout } from './layout';
-export { default as Modal } from './modal';
-export { default as Icon } from './icon';
-export { default as Popconfirm } from './popconfirm';
-export { default as Result } from './result';
-export { default as PageHeader } from './page-header';
 export { default as Alert } from './alert';
-export { default as AutoComplete } from './auto-complete';
 export { default as Anchor } from './anchor';
+export { default as AutoComplete } from './auto-complete';
 export { default as Avatar } from './avatar';
+export { default as BackTop } from './back-top';
 export { default as Badge } from './badge';
+export { default as Breadcrumb } from './breadcrumb';
+export { default as Button } from './button';
+export { default as Calendar } from './calendar';
+export { default as Card } from './card';
 export { default as Carousel } from './carousel';
 export { default as Cascader } from './cascader';
+export { default as Checkbox } from './checkbox';
+export { Col } from './grid';
 export { default as Collapse } from './collapse';
+// common-skip
+// config-provider-skip
+export { default as DatePicker } from './date-picker';
 export { default as Descriptions } from './descriptions';
+export { default as Divider } from './divider';
+export { default as Drawer } from './drawer';
+export { default as Dropdown } from './dropdown';
 export { default as Form } from './form';
-export { Row, Col } from './grid';
+// grid-skip
+export { default as Icon } from './icon';
+export { default as Image } from './image';
 export { default as Input } from './input';
 export { default as InputNumber } from './input-number';
+export { default as Layout } from './layout';
 export { default as List } from './list';
+export { default as Menu } from './menu';
+export { default as Message } from './message';
+export { default as Modal } from './modal';
+export { default as Notification } from './notification';
+export { default as PageHeader } from './page-header';
+export { default as Pagination } from './pagination';
+export { default as Popconfirm } from './popconfirm';
+export { default as Popover } from './popover';
+export { default as Progress } from './progress';
 export { default as Radio } from './radio';
 export { default as Rate } from './rate';
+export { default as Result } from './result';
+export { Row } from './grid';
 export { default as Select } from './select';
+export { default as Skeleton } from './skeleton';
 export { default as Slider } from './slider';
+export { default as Spin } from './spin';
 export { default as Statistic } from './statistic';
+export { default as Steps } from './steps';
+// style-skip
 export { default as Switch } from './switch';
 export { default as Table } from './table';
+export { default as Tabs } from './tabs';
+export { default as Tag } from './tag';
+export { default as Timeline } from './timeline';
 export { default as Tooltip } from './tooltip';
 export { default as Transfer } from './transfer';
 export { default as Tree } from './tree';
 export { default as TreeSelect } from './tree-select';
-export { default as Dropdown } from './dropdown';
-export { default as Popover } from './popover';
-export { default as Progress } from './progress';
-export { default as Spin } from './spin';
-export { default as Divider } from './divider';
-
-export { default as DatePicker } from './date-picker';
-export { default as Calendar } from './calendar';
-export { default as Drawer } from './drawer';
-export { default as Message } from './message';
-export { default as Notification } from './notification';
-export { default as Upload } from './upload';
-export { default as Pagination } from './pagination';
-export { default as Steps } from './steps';
 export { default as Typography } from './typography';
-export { default as Menu } from './menu';
-export { default as BackTop } from './back-top';
-export { default as Checkbox } from './checkbox';
-export { default as Skeleton } from './skeleton';
-export { default as Timeline } from './timeline';
-export { default as Breadcrumb } from './breadcrumb';
-export { default as Image } from './image';
+export { default as Upload } from './upload';
