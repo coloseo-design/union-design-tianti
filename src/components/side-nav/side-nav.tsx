ef1234293ceebccc29086e781cc01d3cfa73431d
liao.yj <liao.yingju@cyberway.net.cn>
2023-08-04 16:05:41 +0800

    side-nav 修改四级菜单样式

--- src/components/side-nav/side-nav.tsx
+++ src/components/side-nav/side-nav.tsx
@@ -365,7 +365,9 @@ export default class SideNav<Data> extends BaseComponent<
         onClick={() => this.onClickViewItemInline(data)}
       >
         {icon && <div className={this.gpc("icon")}>{icon}</div>}
-        <div className={this.gpc("text")}>{nameExtractor(data)}</div>
+        <div className={this.gpc("text")}>
+          {nameExtractor(data)}
+        </div>
         {hasChildren && (
           <div className={this.gpc("arrow")}>
             <Icon type={arrowIconType} style={{ fontSize: 14 }} />
@@ -445,7 +447,10 @@ export default class SideNav<Data> extends BaseComponent<
         }
       >
         {icon && <div className={this.gpc("icon")}>{icon}</div>}
-        <div className={this.gpc("text")}>{nameExtractor(data)}</div>
+        <div className={this.gpc("text")}>
+          {level === 4 ? '- ' : null}
+          {nameExtractor(data)}
+        </div>
         {level === 1 && hasChildren && (
           <div className={this.gpc("arrow")}>
             <Icon type="right2-line" style={{ fontSize: 14 }} />
